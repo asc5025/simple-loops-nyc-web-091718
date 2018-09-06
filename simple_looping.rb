f869a222c61727ec1af9dabf5f6b244743e64fc5
Albert Chun <albert.s.chun@gmail.com>
2018-09-06 21:12:35 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -56,6 +56,7 @@ def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times = 1..7
   for number in number_of_times
-    puts 
+    puts phrase
+  end
 end
 
